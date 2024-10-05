ebf6a3ab81793a8a041c3e30336c70a352d4afe7
magdlenar <rohmannawatimagdalena@gmail.com>
2024-10-05 15:58:41 +0700

    Menambahkan folder baru

---
+++ lib/model/produk.dart
@@ -0,0 +1,17 @@
+class Produk {
+  int? id;
+  String? kodeProduk;
+  String? namaProduk;
+  int? hargaProduk;
+  Produk({this.id, this.kodeProduk, this.namaProduk, this.hargaProduk});
+
+  factory Produk.fromJson(Map<String, dynamic> obj) {
+    return Produk(
+      id: obj['id'] is String ? int.tryParse(obj['id']) : obj['id'],
+      kodeProduk: obj['kode_produk'],
+      namaProduk: obj['nama_produk'],
+      hargaProduk:
+          obj['harga'] is String ? int.tryParse(obj['harga']) : obj['harga'],
+    );
+  }
+}
